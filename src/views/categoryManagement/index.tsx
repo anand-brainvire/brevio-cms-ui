4b2cb5296e6ff5311f654b81ba95e68552c69d57
Parth Patel <parth.patel@brainvire.com>
2025-06-20 12:42:11 +0530

    TASK-000000: Updated the column sequence in categories[Status: done]

--- src/views/categoryManagement/index.tsx
+++ src/views/categoryManagement/index.tsx
@@ -32,8 +32,8 @@ const categoryManagement = (): ReactElement => {
 
     const COL_ARR_CATEGORY = [
         { name: t('Name'), sortable: true, fieldName: 'category_translations', type: 'multilang', translationKey: 'name' },
+        { name: t('Slug'), sortable: true, fieldName: 'slug', type: 'text' },
         { name: t('Description'), sortable: false, fieldName: 'category_translations', type: 'multilang', translationKey: 'description' },
-        { name: t('Slug'), sortable: false, fieldName: 'slug', type: 'text' },
         { name: t('Status'), sortable: true, fieldName: 'is_active', type: 'status', headerCenter: 'true' },
     ] as ColArrType[];
 
